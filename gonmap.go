b84506e9307955c0b4a47b97c79b7db94daf5045
Joe Patrick <joewpat@gmail.com>
2021-03-26 11:53:11 -0400

    initializing git repo

---
+++ gonmap.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Println("go nmap")
+	results, err := getIPv4()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(results)
+}
+
+//func scan(ip string) []string {
+//hosts := []string
+//	fmt.Println(getIPv4())
+//	return "scan"
+//}
+
+func getIPv4() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+			return ip.String(), nil
+		}
+	}
+	return "", errors.New("no IPV4 address found")
+}
