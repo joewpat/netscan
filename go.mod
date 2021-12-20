8bd6ab3744d7bc4c111daf65d7edc4dea0e6566d
Joe Patrick <joewpat@gmail.com>
2021-12-20 18:55:39 -0500

    concurrency test

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/digineo/go-ping
 go 1.16
 
 require (
-
 	github.com/go-ping/ping v0.0.0-20210327002015-80a511380375
 	github.com/mostlygeek/arp v0.0.0-20170424181311-541a2129847a
 )
--- gonmap.go
+++ gonmap.go
@@ -3,9 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"net"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/go-ping/ping"
@@ -17,34 +19,50 @@ func main() {
 	fmt.Printf("gonmap - lightweight network scanner utility\n\n")
 	ipv4address, err := getIPv4()
 	if err != nil {
-		panic(err)
+		log.Println(err)
+		return
 	}
 	valid := isValidIpv4(ipv4address)
-	if valid != true {
+	if !valid {
 		fmt.Println("error: unable to locate valid ipv4 address")
 	}
 	fmt.Printf("IPv4 LAN address:\t%v\n", ipv4address)
 	targets := getTargets(ipv4address)
-	hostsAvailable := 0
-	for _, v := range targets {
-		l, r := testConnection(v)
-		if l <= 99 { //filter out down clients
-			host, _ := net.LookupAddr(v) //attempt hostname lookup
-			if len(host) == 0 {
-				host = append(host, "N/A\t")
+	hostsAvailable := 0 // This variable is closed by the anon goroutine.  It will result in a data race without some form of mutual exclusion.
+	mu := sync.Mutex{}  // Write mutex to lock access to the closed counter variable
+	wg := sync.WaitGroup{}
+	sem := make(chan struct{}, 256) // The buffer capacity determines the size of the semaphore.
+	for _, v := range targets {     // The range loop will continue to start new goroutines until the channel is full
+		sem <- struct{}{} // When the channel is full, execution will block here until a struct is read out of the channel (bounded parallelism)
+		wg.Add(1)         // increments wg counter
+		go func(ip string) {
+			defer func() {
+				<-sem     // when the channel is full, removing a struct{}{} from the semaphore channel allow a new goroutine to be started
+				wg.Done() // decrements wg counter
+			}()
+			l, r := testConnection(ip)
+			if l <= 99 { //filter out down clients
+				host, _ := net.LookupAddr(ip) //attempt hostname lookup
+				if len(host) == 0 {
+					host = append(host, "N/A\t")
+				}
+				m := arp.Search(ip) // get MAC address
+				if m == "" {
+					m = "N/A\t\t"
+				}
+				fmt.Println("Host: ", ip, "\tRTT: ", r, "\tMAC:\t", m, "\tHostname:", host[0])
+				mu.Lock()         // The mutex locks access to the counter variable preventing simultaneous writes to the same location in memory (data race).
+				defer mu.Unlock() // unlock the mutex when it is safe to write to this memory location again.
+				hostsAvailable++  // This counter is a perfect example of a data race.  Multiple goroutines could potentially write to it simultaneously
 			}
-			m := arp.Search(v) // get MAC address
-			if m == "" {
-				m = "N/A\t\t"
-			}
-			fmt.Println("Host: ", v, "\tRTT: ", r, "\tMAC:\t", m, "\tHostname:", host[0])
-			hostsAvailable++
-		}
+
+		}(v)
 	}
+	wg.Wait() // will block until wg counter reaches zero
 	total := len(targets)
 	t1 := time.Now()
 	elapsed := t1.Sub(t0)
-	d, _ := time.ParseDuration("1s")
+	d, _ := time.ParseDuration("1us")
 	fmt.Printf("\nTargets Scanned: %v\tUp: %v\nTotal Scan Time: %v\n\n", total, hostsAvailable, elapsed.Round(d))
 }
 
@@ -56,15 +74,17 @@ func testConnection(target string) (float64, time.Duration) {
 	timeout := time.Millisecond * 100
 	pinger, err := ping.NewPinger(target)
 	if err != nil {
-		panic(err)
+		log.Println(err)
+		return 0, time.Duration(0)
 	}
 	pinger.Timeout = timeout
 	pinger.Count = 1
 	err = pinger.Run() // blocks until finished
 	if err != nil {
-		panic(err)
+		log.Println(err)
+		return 0, time.Duration(0)
 	}
-	stats := pinger.Statistics() // get send/receive/rtt stats | TODO: get hostnames if possible
+	stats := pinger.Statistics() // get send/receive/rtt stats
 	d, _ := time.ParseDuration("0.01ms")
 	return stats.PacketLoss, stats.AvgRtt.Round(d)
 }
